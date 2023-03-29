d57c0764f45582e9b298ffcb3dd6e1b0c9fa4286
Aman Kumar <amanmandal644@gmail.com>
2023-03-30 02:09:53 +0530

    Working on Dark Mode

--- Frontend/scripts/admin.js
+++ Frontend/scripts/admin.js
@@ -1,13 +1,29 @@
 // ------------------- Login Part ----------------------
 
-const SubmitButton = document.querySelector('.submit');
+const SubmitButton = document.querySelector(".submit");
 
-const UserName = document.querySelector('#username');
-const Password = document.querySelector('#password');
+const UserName = document.querySelector("#username");
+const Password = document.querySelector("#password");
+
+SubmitButton.addEventListener("click", () => {
+  UserName.value == "admin" && Password.value == "hairsalon"
+    ? (alert("Welcome Admin"), ((UserName.value = ""), (Password.value = "")))
+    : alert("Wrong Credentials");
+});
 
 
-SubmitButton.addEventListener('click', () =>{
-    UserName.value == "admin" && Password.value == "hairsalon" ? 
-    (alert('Welcome Admin'), (UserName.value="", Password.value="")) : 
-    alert('Wrong Credentials');
-})
+// ------------------- DarkMode Part ----------------------
+
+const Moon = document.querySelector("#moon");
+const HTML = document.querySelector('html');
+const LoginBox = document.querySelector('.login-box');
+
+Moon.addEventListener("click", (e) => {
+  if (Moon.className == "bx bx-moon") {
+    e.target.className = "bx bx-sun";
+    HTML.style.background="white";
+    LoginBox.id = "lg";
+  } else {
+     window.location.reload();
+  }
+});
