af4ced0aee9231c2cb9ea32d5d91fa6eab3a0fd4
07.Mayank-raj <2003.mayank.raj@gmail.com>
2023-03-30 19:32:02 +0530

    added swal for register pages

--- Frontend/routes/loginSignup/js/signup.js
+++ Frontend/routes/loginSignup/js/signup.js
@@ -3,40 +3,68 @@ const baseurl = "http://localhost:8080/"
 import loading from "../components/loading.components.js";
 import hideLoading from "../components/hideLoading.components.js";
 
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'bottom-end',
+    showConfirmButton: false,
+    timer: 2000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
 
+
+  Toast.fire({
+    icon: 'success',
+    title: 'welcome to register page'
+  })
+
+  
 const singupfrom = document.getElementById('register-form');
-singupfrom.addEventListener('submit',(e)=>{
+singupfrom.addEventListener('submit', (e) => {
     e.preventDefault();
     let email = document.getElementById("email").value
     let name = document.getElementById("name").value
     let password = document.getElementById("pass").value
-    const user = {name, email, password}
+    const user = { name, email, password }
     loading()
     register(user)
 
     console.log(user);
 })
 
-const register = async(user) =>{
+const register = async (user) => {
     try {
 
-        const res = await fetch(`${baseurl}user/register`,{
-            method : "POST",
-            headers : {
-                'Content-Type' : 'application/json'
+        const res = await fetch(`${baseurl}user/register`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
             },
-            body : JSON.stringify(user) 
+            body: JSON.stringify(user)
         })
         let data = await res.json()
-        if(data.err){
-            alert(data.err)
+        if (data.err) {
+           
+              
+             await Toast.fire({
+                icon: 'error',
+                title: 'Error while registering'
+              })
             hideLoading()
             window.location.reload();
 
         }
-        else{
-            alert(user.name + " user created successfully")  
-            window.location.href = "./login.html"  
+        else {
+                          
+             await Toast.fire({
+                icon: 'success',
+                title: 'User created successfully'
+              })
+
+            window.location.href = "./login.html"
         }
     } catch (error) {
         console.log(error.message);
