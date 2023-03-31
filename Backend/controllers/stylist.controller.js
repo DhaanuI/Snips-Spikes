ede2c53c47986ba8992579060fb27285dab700a0
Aman Kumar <amanmandal644@gmail.com>
2023-03-31 14:57:02 +0530

    Stylist Routes and Controllers Added

---
+++ Backend/controllers/stylist.controller.js
@@ -0,0 +1,71 @@
+
+// --------------->>>>>>>> Male Service Model Location <<<<<<<<-------------------
+const { StylistModel } = require("../model/stylist.model");
+
+
+// --------->>>> GET <<<<<---------
+const StylistGetData = async (req, res) => {
+    try {
+        const data = await StylistModel.find()
+        res.status(202).send(data);
+
+    } catch (error) {
+        res.status(404).send({
+            Message: "Bad request 404",
+        });
+    }
+}
+
+// --------->>>> POST <<<<<---------
+const StylistPostData = async (req, res) => {
+    const payload = req.body;
+    try {
+        const data = new StylistModel(payload);
+        await data.save();
+        //console.log(data);
+
+        res.status(202).send({
+            Message: "Data saved successfully",
+        });
+
+    } catch (error) {
+        res.status(404).send({
+            Message: "Bad request 404",
+        });
+    }
+}
+
+// --------->>>> PATCH <<<<<---------
+const StylistPatchData = async (req, res) => {
+    const ID = req.params.id;
+    const payload = req.body
+    try {
+        await StylistModel.findByIdAndUpdate({ _id: ID }, payload)
+        res.status(202).send({
+            Message: "Data successfully modified",
+        });
+    }
+    catch (error) {
+        res.status(404).send({
+            Message: "Bad request 404",
+        });
+    }
+}
+
+// --------->>>> DELETE <<<<<---------
+const StylistDeleteData = async (req, res) => {
+    const ID = req.params.id;
+    try {
+        await StylistModel.findByIdAndDelete({ _id: ID })
+        res.status(202).send({
+            Message: "Data successfully deleted",
+        });
+    }
+    catch (error) {
+        res.status(404).send({
+            Message: "Bad request 404",
+        });
+    }
+}
+
+module.exports = {  StylistGetData, StylistPostData, StylistPatchData, StylistDeleteData }
